6f34eda33fdaec0afc60064357644abc92abf2d3
wnloved <khryu02@bk.ru>
2024-10-04 23:09:59 +0500

    index.html fix 2.0

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     path('index.html', views.index, name='index'),
     path('about.html', views.about, name='about'),
     path('archive.html', views.archive, name='archive'),
